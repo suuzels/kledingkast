01c0b741a42c953dda105636734546ceeb551856
suuzels <suzannevdmeer@gmail.com>
2018-02-16 10:51:51 +0100

    added methods
    to throw stuff in the laundry bag and wash everything from the laundry bag.

--- src/main/java/com/capgemini/Clothing.java
+++ src/main/java/com/capgemini/Clothing.java
@@ -1,5 +1,7 @@
 package com.capgemini;
 
+import java.util.ArrayList;
+
 public class Clothing {
 
     public Clothing(String typeOfGarment) {
@@ -7,4 +9,24 @@ public class Clothing {
     }
 
     private String typeOfGarment;
+
+    private boolean isDirty;
+
+    ArrayList<Clothing> laundryBag = new ArrayList<Clothing>();
+
+    public void throwInLaundryBag(Clothing clothing){
+        laundryBag.add(clothing);
+    }
+
+    public void washClothes(ArrayList<Clothing> laundryBag){
+        System.out.println("Your clothes are being washed.");
+        for (Clothing clothes : laundryBag){
+            isDirty = true;
+        }
+        for (Clothing clothes : laundryBag){
+            laundryBag.remove(clothes);
+        }
+        System.out.println("All the clothes from the laundry bag are now clean again.");
+        
+    }
 }
